a0a3e84a60ed8bad56c9260a28f43b4ff057c1d4
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-09-14 05:55:28 -0400

    Game Input
    Made code to reset level and quit game

---
+++ Assets/Ability Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameController : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        { //If you press R
+            SceneManager.LoadScene("AbilityTest"); 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+}
