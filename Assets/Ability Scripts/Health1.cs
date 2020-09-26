64abaf574aa70282b99d5ea8dad7c9cb212f9f0c
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-09-26 15:40:14 -0400

    part C
    Created health script, damage script, and health bar for player

---
+++ Assets/Ability Scripts/Health1.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health1 : MonoBehaviour
+{
+    [SerializeField] int maxHealth;
+    [SerializeField] int currentHealth;
+
+    [SerializeField] Slider healthBar;
+
+
+
+    void Start()
+    {
+        healthBar.maxValue = maxHealth;
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = currentHealth;
+    }
+
+
+    public void healPlayer(int healAmount)
+    {
+        currentHealth += healAmount;
+
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public void hurtPlayer(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+}
---
+++ Assets/Ability Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    // Knockback
+
+    [SerializeField] float knockBackForce;
+    [SerializeField] float knockBackTime;
+    private float knockBackCounter;
+
+    //
+
+        
+
+
+
+    public CharacterController controller;
+    public Transform cam;
+    public float speed;
+
+    public float walkSpeed = 6f;
+    public float sprintSpeed = 20f;
+
+    public float turnSmoothTime = 0.1f;
+
+    public Animator anim;
+
+    
+
+    float turnSmoothVelocity;
+
+    public float jumpSpeed = 30.0f;
+    public float gravity = 20.0f;
+
+    private void Start()
+    {
+        
+        
+
+    }
+
+    void Update()
+    {
+
+        //
+
+    
+
+
+
+            //
+
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+
+            if (vertical < 0)
+            {
+                vertical = 0;
+            }
+
+            if (controller.isGrounded)
+            {
+                anim.SetBool("Jumping", false);
+
+                if (direction.magnitude >= 0.1f)
+                {
+
+
+                    float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+                    Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                    controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+                    bool move = (vertical > 0) || (horizontal != 0);
+
+                    anim.SetBool("IsRunning", true);
+
+                }
+                else
+                {
+                    anim.SetBool("IsRunning", false);
+                }
+
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    direction.y = jumpSpeed;
+
+                    anim.SetBool("Jumping", true);
+                }
+
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    speed = sprintSpeed;
+                    anim.SetBool("Sprinting", true);
+                }
+                else
+                {
+                    speed = 6f;
+                    anim.SetBool("Sprinting", false);
+                }
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    anim.SetBool("Attacking", true);
+                }
+                else
+                {
+                    anim.SetBool("Attacking", false);
+                }
+
+            }
+
+
+            direction.y -= gravity * Time.deltaTime;
+            controller.Move(direction * Time.deltaTime);
+
+        
+
+
+    }
+
+
+
+   
+}
