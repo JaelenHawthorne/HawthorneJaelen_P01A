3ef1357b5d7eeef4a55f26f7a1614e23e3b5d98d
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-09-28 00:23:16 -0400

    visuals
    Added more audio and particles to player actions

--- Assets/Ability Scripts/ThirdPersonMovement.cs
+++ Assets/Ability Scripts/ThirdPersonMovement.cs
@@ -7,13 +7,15 @@ using UnityEngine.UI;
 public class ThirdPersonMovement : MonoBehaviour
 {
 
-    // Knockback
+    /// parrticles
 
-    [SerializeField] float knockBackForce;
-    [SerializeField] float knockBackTime;
-    private float knockBackCounter;
+    public GameObject Shockwave;
+    public GameObject Sparks;
 
-    //
+    public GameObject Hearts;
+    public GameObject HitEffect;
+
+    /// 
 
     public AudioSource sounds;
     public AudioClip heal;
@@ -34,7 +36,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public Animator anim;
 
-    
+
 
     float turnSmoothVelocity;
 
@@ -51,6 +53,8 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
 
+
+
         //
 
 
@@ -59,82 +63,83 @@ public class ThirdPersonMovement : MonoBehaviour
         //
 
         float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
-            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        
 
-            if (vertical < 0)
+
+        if (vertical < 0)
+        {
+            vertical = 0;
+        }
+
+        if (controller.isGrounded)
+        {
+            anim.SetBool("Jumping", false);
+
+            if (direction.magnitude >= 0.1f)
             {
-                vertical = 0;
-            }
 
-            if (controller.isGrounded)
-            {
-                anim.SetBool("Jumping", false);
 
-                if (direction.magnitude >= 0.1f)
-                {
-
-             
 
 
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
 
-                    Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                    controller.Move(moveDir.normalized * speed * Time.deltaTime);
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
-                    bool move = (vertical > 0) || (horizontal != 0);
+                bool move = (vertical > 0) || (horizontal != 0);
 
-                    anim.SetBool("IsRunning", true);
+                anim.SetBool("IsRunning", true);
 
-                }
-                else
-                {
-                    anim.SetBool("IsRunning", false);
-                }
-
-
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    direction.y = jumpSpeed;
-
-                    anim.SetBool("Jumping", true);
-
-                sounds.clip = jump;
-                sounds.PlayOneShot(sounds.clip);
-                }
-
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    speed = sprintSpeed;
-                    anim.SetBool("Sprinting", true);
-                }
-                else
-                {
-                    speed = 6f;
-                    anim.SetBool("Sprinting", false);
-                }
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    anim.SetBool("Attacking", true);
-                }
-                else
-                {
-                    anim.SetBool("Attacking", false);
-                }
-
+            }
+            else
+            {
+                anim.SetBool("IsRunning", false);
             }
 
 
-            direction.y -= gravity * Time.deltaTime;
-            controller.Move(direction * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                direction.y = jumpSpeed;
 
-        
+                anim.SetBool("Jumping", true);
+
+                sounds.clip = jump;
+                sounds.PlayOneShot(sounds.clip);
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = sprintSpeed;
+                anim.SetBool("Sprinting", true);
+            }
+            else
+            {
+                speed = 6f;
+                anim.SetBool("Sprinting", false);
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetBool("Attacking", true);
+            }
+            else
+            {
+                anim.SetBool("Attacking", false);
+            }
+
+        }
+
+
+        direction.y -= gravity * Time.deltaTime;
+        controller.Move(direction * Time.deltaTime);
+       
+
+
 
 
     }
@@ -149,17 +154,20 @@ public class ThirdPersonMovement : MonoBehaviour
     public void Hurt()
     {
         anim.SetTrigger("Hurt");
+        Instantiate(HitEffect, transform.position, transform.rotation);
     }
 
     public void Cure()
     {
         anim.SetTrigger("Heal");
+        Instantiate(Hearts, transform.position, transform.rotation);
     }
 
- private void Fall()
+    private void Fall()
     {
         AudioClip fall = land;
         sounds.PlayOneShot(fall);
+        Instantiate(Shockwave, transform.position, transform.rotation);
 
     }
 
@@ -167,6 +175,7 @@ public class ThirdPersonMovement : MonoBehaviour
     {
         AudioClip step = walk;
         sounds.PlayOneShot(step);
+        Instantiate(Sparks, transform.position, transform.rotation);
 
     }
 
@@ -176,4 +185,5 @@ public class ThirdPersonMovement : MonoBehaviour
         sounds.PlayOneShot(die);
 
     }
+
 }
